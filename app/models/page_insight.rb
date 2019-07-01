23a97e00916f5cfb5dc112cb052055d8601c1891
Myroslava Stavnycha <stavnycha.m@gmail.com>
2019-07-01 18:48:44 +0300

    page insight model validations

--- app/models/page_insight.rb
+++ app/models/page_insight.rb
@@ -1,3 +1,10 @@
 class PageInsight < ApplicationRecord
   validates :url, presence: true
+
+  validates :max_ttfb,
+            :max_tti,
+            :max_speed_index,
+            :max_ttfp,
+            presence: true,
+            numericality: { greater_than: 0 }
 end
